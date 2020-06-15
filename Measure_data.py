0e9b22e82789311916689df1b577223441c5236b
xavierschm <66959584+xavierschm@users.noreply.github.com>
2020-06-15 16:08:52 +0100

    Add files via upload

---
+++ Measure_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 11:41:19 2020
+
+@author: ID107354
+"""
+
+class Measure_data:
+    
+    def __init__(self, measure_data_df, measure_object):
+        
+        self.data = measure_data_df
+        self.measure = measure_object
+        

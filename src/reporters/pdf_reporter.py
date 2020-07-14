934eef299064b672b043c7496471f0a7eb0b7bc7
alexZaicev <alex.zaicef@gmail.com>
2020-07-14 16:04:58 +0100

    financial & hr scorecard impl

--- src/reporters/pdf_reporter.py
+++ src/reporters/pdf_reporter.py
@@ -47,11 +47,11 @@ class PDFReporter(RGReporterBase):
                            dest='F')
 
     def do_compose(self, options=None):
-        # self.__do_compose_cpm_scorecard(options)
-        # self.__do_compose_grid_charts(options)
-        # self.__do_compose_sdm_scorecard(options)
+        self.__do_compose_cpm_scorecard(options)
+        self.__do_compose_grid_charts(options)
+        self.__do_compose_sdm_scorecard(options)
         self.__do_compose_financial_hr_scorecard(options)
-        # self.__do_compose_relationship_effectiveness_scorecard(options)
+        self.__do_compose_relationship_effectiveness_scorecard(options)
 
     def __do_compose_relationship_effectiveness_scorecard(self, options):
         h = self.__create_scorecard_top(add_page=True, h=2.25, w=405)
